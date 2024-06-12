1b2721aae81af181178419ab23662c0c7b300d0c
TheJavaDrinker <135500330+TheJavaDrinker@users.noreply.github.com>
2024-06-12 16:54:47 +1000

    Started adding functions to the ruby file.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
+print "Please enter a number: "
+number1 = gets.chomp.to_i
 
+print "Please enter another number"
+number2 = gets.chomp.to_i
+
+sum = number1 + number2
+
+puts "The result of adding #{num1} and #{num2} is: #{sum}"
